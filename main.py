40e91ec03582723c06acf9e94bae98473fdc9941
Tryfolos <70145934+Tryfolos@users.noreply.github.com>
2020-10-16 09:25:26 +0200

    change

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-import time
+###################################################################################################################
+print("Välkommen till kundservice.")
+print("Vi erbjuder tre olika abbonemang. 'Kontant', 'Normal' och 'Plus'")
+print("")
+input("Skriv något här och tryck på enter för att fortsätta.")
+
+print("")
+print("")
 
 
+print("Vi hjälper dig avgöra vilket av de tillgängliga abbonemangen som är bäst för just ditt behov.")
+print("")
+input("Skriv något här och tryck på enter för att fortsätta.")
 
-print("Välkommen till kundservice. Vi hjälper dig avgöra\nvilket av de tillgängliga abbonemangen som är bäst för ditt behov.")
+
+print("")
+print("")
+
+print("Alla svar som du ger bör endast anges i siffor. Du skall alltså lämna ute bokstäver och specialtecken.")
+print("Dina svar skall även vara angivna i form av heltal.")
+print("")
+input("Skriv något här och tryck på enter för att fortsätta.")
+
+print("")
+print("")
+
+
 svar = input("Hur många minuter ringer du varje månad?")
+
 
 if svar == "":
 	pass
