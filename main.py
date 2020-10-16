eb6038859e92a99d840da72fd0d3b8dfa1df2dcc
Tryfolos <70145934+Tryfolos@users.noreply.github.com>
2020-10-16 09:12:15 +0200

    change

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+
+
+print("Välkommen till kundservice. Vi hjälper dig avgöra\nvilket av de tillgängliga abbonemangen som är bäst för ditt behov.")
+svar = input("Hur många minuter ringer du varje månad?")
+
+if svar == "":
+	pass
