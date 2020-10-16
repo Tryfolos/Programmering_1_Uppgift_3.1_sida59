e8ee78a29456ece0e7d5d1104450068378c2b103
Tryfolos <70145934+Tryfolos@users.noreply.github.com>
2020-10-16 09:37:42 +0200

    change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ###################################################################################################################
+import time
+
 print("Välkommen till kundservice.")
-print("Vi erbjuder tre olika abbonemang. 'Kontant', 'Normal' och 'Plus'")
+print("Vi erbjuder tre olika abonnemang. 'Kontant', 'Normal' och 'Plus'")
 print("")
 input("Skriv något här och tryck på enter för att fortsätta.")
 
@@ -8,7 +10,7 @@ print("")
 print("")
 
 
-print("Vi hjälper dig avgöra vilket av de tillgängliga abbonemangen som är bäst för just ditt behov.")
+print("Vi hjälper dig avgöra vilket av de tillgängliga abonnemangen som är bäst för just ditt behov.")
 print("")
 input("Skriv något här och tryck på enter för att fortsätta.")
 
@@ -16,7 +18,7 @@ input("Skriv något här och tryck på enter för att fortsätta.")
 print("")
 print("")
 
-print("Alla svar som du ger bör endast anges i siffor. Du skall alltså lämna ute bokstäver och specialtecken.")
+print("Svaret du ger bör endast anges i siffor. Du skall alltså lämna ute bokstäver och specialtecken.")
 print("Dina svar skall även vara angivna i form av heltal.")
 print("")
 input("Skriv något här och tryck på enter för att fortsätta.")
@@ -28,5 +30,15 @@ print("")
 svar = input("Hur många minuter ringer du varje månad?")
 
 
-if svar == "":
-	pass
+svar = int(svar)
+
+if svar <= 33:
+	print("Kontant är det bästa abonnemanget för dig!")
+
+if svar > 33 and svar < 67:
+	print("Normal är det bästa abonnemanget för dig!")
+
+if svar > 66:
+	print("Plus är det bästa abonnemanget för dig!")
+
+time.sleep(2)
